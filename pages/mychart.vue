751a4db84a8e980feb26a108b23f0f6bf959e8d5
betty <xu3ru8u6@i-mps.com>
2022-08-11 18:00:41 +0800

    feat(mychart): 畫面修正
    descriptions:
    -
    
    issue

--- pages/mychart.vue
+++ pages/mychart.vue
@@ -1,24 +1,28 @@
 <template>
-<div>
-    <!-- <div>
+<div class="mychart">
+    <div class="chart-title">
         前端薪資調查報告
-    </div> -->
-    <!-- <div class = "bar_part">
-        <Bar
-            :chart-options="chartOptions"
-            :chart-data="chartData"
-            :chart-id="chartId"
-            :dataset-id-key="datasetIdKey"
-            :plugins="plugins"
-            :css-classes="cssClasses"
-            :styles="styles"
-            :width="350"
-            :height="200"
-        />
     </div>
-    <button @click="changeData()">點我改值</button> -->
-    <!-- <div class = "bar_part">
-        <Pie
+    <div class="items">
+      <div class="items-inner">
+        <div class="item choose-item">
+          基本資料
+        </div>
+        <div class="item">
+          薪資與產業
+        </div>
+      </div>
+      <!-- <div>
+        下拉選單
+      </div> -->
+    </div>
+
+    <!-- 地區 -->
+    <div class="row">
+      <div class="bg-chart">
+        <div class="bg-chart-inner">
+          <div class="chart-outer">
+            <Bar
             :chart-options="chartOptions"
             :chart-data="chartData"
             :chart-id="chartId"
@@ -26,10 +30,52 @@
             :plugins="plugins"
             :css-classes="cssClasses"
             :styles="styles"
-            :width="150"
+            :width="400"
+            :height="300"
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- 年齡 性別比 -->
+    <div class="row">
+      <div class="bg-chart col-md">
+        <div class="bg-chart-inner">
+          <div class="chart-outer">
+            <Bar
+            :chart-options="chartOptions"
+            :chart-data="chartData"
+            :chart-id="chartId"
+            :dataset-id-key="datasetIdKey"
+            :plugins="plugins"
+            :css-classes="cssClasses"
+            :styles="styles"
+            :width="200"
             :height="150"
-        />
-    </div> -->
+            />
+          </div>
+        </div>
+      </div>
+      <div class="bg-chart col-md">
+        <div class="bg-chart-inner">
+          <div class="chart-outer">
+            <Pie
+              :chart-options="chartOptions"
+              :chart-data="chartData"
+              :chart-id="chartId"
+              :dataset-id-key="datasetIdKey"
+              :plugins="plugins"
+              :css-classes="cssClasses"
+              :styles="styles"
+              :width="150"
+              :height="100"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
 
 
@@ -110,7 +156,74 @@ export default {
 
 .bar_part{
     width: 30%;
-    height:auto ;
+    height:100vh ;
+}
+
+.mychart{
+  width: 100%;
+  padding: 20% ;
+  background-color: rgb(247, 64, 95);
+}
+
+.chart-title{
+  width:100%;
+  margin: 0 auto ;
+  display: flex;
+  justify-content: center;  
+  margin-bottom: 20px;
+}
+
+
+
+/*  */
+
+.items{
+  width:100%;
+  display: flex;
+  justify-content: center;  
+  
+}
+
+.items-inner{
+  display: flex;
+  border: 1px white solid;
+}
+
+.items-inner .item{
+  padding: 10px;
+  margin: 10px;
+}
+
+.choose-item{
+  background: pink;
+  border-radius: 10px;
+}
+
+/*  */
+.bg-chart{
+  margin-top:20px
+}
+.bg-chart-inner{
+  background-color: pink;
+  padding: 20px;
+  width: 100%;
+  /* height:auto; */
+  min-height:100%;
+  display:flex ;
+  justify-content: center;
+  align-items: center;
+  
+}
+
+.chart-outer{
+  width: 80%;
+  height: 80%;
 }
 
 </style>
+
+<!-- 參考畫面 -->
+<!-- https://kuanfuchen.github.io/vuedatagraph/ -->
+<!-- https://the-f2e-week4.chill404.cc/ -->
+<!-- 圖表 參考以下 （vue-chartjs） -->
+<!-- https://vue-chartjs.org/examples/#vue-3-charts -->
